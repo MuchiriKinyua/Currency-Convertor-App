f43a33126dc56756f28bba8125469bef65d2a224
MuchiriKinyua <muchirikinyua2000@gmail.com>
2024-12-24 15:49:14 +0300

    Exchange API

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-const dropList = document.querySelectorAll(".drop-list select");
+const dropList = document.querySelectorAll(".drop-list select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
 
 // Iterate through each dropdown
 for (let i = 0; i < dropList.length; i++) {
@@ -16,4 +19,27 @@ for (let i = 0; i < dropList.length; i++) {
         let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
+}
+window.addEventListener("load", () => {
+    getExchangeRate();
+});
+getButton.addEventListener("click", e => {
+    e.preventDefault();
+    getExchangeRate();
+});
+function getExchangeRate(){
+    const amount = document.querySelector(".amount input");
+    exchangeRateTxt = document.querySelector(".exchange-rate");
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0"){
+        amount.val = "1";
+        amountVal = 1;
+    }
+    exchangeRateTxt.innerText = "Getting exchange rate...";
+    let url = `https://v6.exchangerate-api.com/v6/5109cb7ac4605f10342fa09e/latest/${fromCurrency.value}`;
+    fetch(url).then(response => response.json()).then(result => {
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
+    })
 }
